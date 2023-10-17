34d2bea4d3f84e112574289fbad699ef7fb9fa5d
Abdullah Khan <89281472+Abdullahkhan-123@users.noreply.github.com>
2023-10-17 05:58:13 +0500

    ChatGpt_Clone_Update
    10/17/2023 date updated code

--- django-chatbot-main/django_chatbot/chatbot/views.py
+++ django-chatbot-main/django_chatbot/chatbot/views.py
@@ -9,7 +9,7 @@ from .models import Chat
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 
-openai_api_key = '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'
+openai_api_key = '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'
 openai.api_key = openai_api_key
 
 def ask_openai(message):
@@ -24,7 +24,7 @@ def ask_openai(message):
     answer = response.choices[0].message.content.strip()
     return answer
 
-@login_required(login_url='/log/')
+@login_required(login_url='/logins/')
 def chatpage(request):
     chats = Chat.objects.filter(user=request.user)
 
@@ -36,36 +36,6 @@ def chatpage(request):
         chat.save()
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbots.html', {'chats': chats})    
-
-# Create your views here.
-# @login_required(login_url='/log/')
-# def chatbot(request):
-#     chats = Chat.objects.filter(user=request.user)
-
-#     if request.method == 'POST':
-#         message = request.POST.get('message')
-#         response = ask_openai(message)
-
-#         chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
-#         chat.save()
-#         return JsonResponse({'message': message, 'response': response})
-#     return render(request, 'chatbot.html', {'chats': chats})
-
-
-# def login(request):
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         user = auth.authenticate(request, username=username, password=password)
-#         if user is not None:
-#             auth.login(request, user)
-#             return redirect('chatbot')
-#         else:
-#             error_message = 'Invalid username or password'
-#             return render(request, 'login.html', {'error_message': error_message})
-#     else:
-#         return render(request, 'login.html')
-
 
 def log(request):
     if request.method == 'POST':
@@ -103,27 +73,6 @@ def reg(request):
     return render(request,'reg.html')
 
 
-# def register(request):
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         email = request.POST['email']
-#         password1 = request.POST['password1']
-#         password2 = request.POST['password2']
-
-#         if password1 == password2:
-#             try:
-#                 user = User.objects.create_user(username, email, password1)
-#                 user.save()
-#                 auth.login(request, user)
-#                 return redirect('chatbot')
-#             except:
-#                 error_message = 'Error creating account'
-#                 return render(request, 'register.html', {'error_message': error_message})
-#         else:
-#             error_message = 'Password dont match'
-#             return render(request, 'register.html', {'error_message': error_message})
-#     return render(request, 'register.html')
-
 def logout(request):
     auth.logout(request)
-    return redirect('log')
+    return redirect('/logins/')
